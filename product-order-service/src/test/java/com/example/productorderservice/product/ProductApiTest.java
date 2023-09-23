d3dd331e14c98a2c2048f3c05dddd8f4cece8737
qwe5507 <apvmffkdls@gmail.com>
2023-09-23 22:03:04 +0900

    fix: import 제거

--- product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -4,19 +4,13 @@ import com.example.productorderservice.ApiTest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@SpringBootTest
 public class ProductApiTest extends ApiTest {
-//    @Autowired
-//    private ProductService productService;
 
     @Test
     void 상품등록() {

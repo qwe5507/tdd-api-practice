37a2c59c3ba196bd470965e08cc38ff8262c3a2c
qwe5507 <apvmffkdls@gmail.com>
2023-09-23 22:25:33 +0900

    JPA 적용하기

--- product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -1,17 +1,13 @@
 package com.example.productorderservice.product;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Repository
-class ProductRepository {
-    private Long sequence = 0L;
-    private Map<Long, Product> persistence = new HashMap<>();
-
-    public void save(final Product product) {
-        product.assignId(++sequence);
-        persistence.put(product.getId(), product);
-    }
+interface ProductRepository extends JpaRepository<Product, Long> {
+//    private Long sequence = 0L;
+//    private Map<Long, Product> persistence = new HashMap<>();
+//
+//    public void save(final Product product) {
+//        product.assignId(++sequence);
+//        persistence.put(product.getId(), product);
+//    }
 }
--- product-order-service/src/main/java/com/example/productorderservice/product/ProductService.java
+++ product-order-service/src/main/java/com/example/productorderservice/product/ProductService.java
@@ -2,6 +2,7 @@ package com.example.productorderservice.product;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +15,7 @@ class ProductService {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 

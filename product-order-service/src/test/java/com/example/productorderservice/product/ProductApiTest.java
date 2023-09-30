1de30676670738e0b1ad8557ed5338f2d7deae81
qwe5507 <apvmffkdls@gmail.com>
2023-09-30 10:00:50 +0900

    API 테스트로 전환

--- product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -1,13 +1,18 @@
 package com.example.productorderservice.product;
 
 import com.example.productorderservice.ApiTest;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import static com.example.productorderservice.product.ProductSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductApiTest extends ApiTest {
+    @Autowired
+    ProductRepository productRepository;
 
     @Test
     void 상품등록() {
@@ -29,4 +34,13 @@ public class ProductApiTest extends ApiTest {
         assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
     }
 
+    @Test
+    void 상품수정() {
+        ProductSteps.싱픔등록요청(ProductSteps.상품등록요청_생성());
+        final long productId = 1L;
+
+        final ExtractableResponse<Response> response = ProductSteps.상품수정요청(productId);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(productRepository.findById(1L).get().getName()).isEqualTo("상품 수정");
+    }
 }
--- product-order-service/src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ product-order-service/src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,16 +1,11 @@
 package com.example.productorderservice.product;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.Extensions;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
 
+import static com.example.productorderservice.product.ProductSteps.상품수정요청_생성;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -22,7 +17,7 @@ public class ProductServiceTest {
     void 상품수정() {
         productService.addProduct(ProductSteps.상품등록요청_생성());
         final Long productId = 1L;
-        final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
+        final UpdateProductRequest request = 상품수정요청_생성();
 
         productService.updateProduct(productId, request);
 
@@ -31,4 +26,5 @@ public class ProductServiceTest {
         assertThat(productResponse.name()).isEqualTo("상품 수정");
         assertThat(productResponse.price()).isEqualTo(2000);
     }
+
 }
